6cb8f86d84a33da492a82cc0c9c4eeb6c601e11a
Irina Martsulevich <malinovayairiska@gmail.com>
2022-01-10 15:17:31 +0300

    feat: create start page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import { AppBar, Grid, Typography } from '@mui/material';
+
+import { StartInfo } from './components/StartInfo/StartInfo';
+
 export const App = () => {
-  return <div className={'app'}> </div>;
+  return (
+    <Grid container rowSpacing={4} justifyContent={'center'} alignItems={'center'}>
+      <Grid item xs={12}>
+        <AppBar position={'static'}>
+          <Typography
+            variant={'h4'}
+            component={'h1'}
+            align={'center'}
+            sx={{ padding: 2 }}
+          >
+            Todolist
+          </Typography>
+        </AppBar>
+      </Grid>
+      <StartInfo />
+    </Grid>
+  );
 };
