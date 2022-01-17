f9e999ba683a4526b2c3ec05b0f5f28871b5118b
Irina Martsulevich <malinovayairiska@gmail.com>
2022-01-17 15:13:37 +0300

    refactor: implement styles and actions for filter buttons by loop

--- src/components/SortBar/SortBar.jsx
+++ src/components/SortBar/SortBar.jsx
@@ -10,61 +10,27 @@ export const SortBar = ({ id, activeFilter }) => {
   const styles = getStyles();
   const dispatch = useDispatch();
 
-  const handleFilterAllClick = () => dispatch(filterTasks(id, TODOLIST_FILTERS.ALL));
-  const handleFilterTodoClick = () => dispatch(filterTasks(id, TODOLIST_FILTERS.TODO));
-  const handleFilterProgressClick = () =>
-    dispatch(filterTasks(id, TODOLIST_FILTERS.PROGRESS));
-  const handleFilterDoneClick = () => dispatch(filterTasks(id, TODOLIST_FILTERS.DONE));
-
-  const filterAllButtonFinalClass =
-    activeFilter === TODOLIST_FILTERS.ALL
-      ? { ...styles.filterButton, ...styles.active }
-      : styles.filterButton;
-
-  const filterTodoButtonFinalClass =
-    activeFilter === TODOLIST_FILTERS.TODO
-      ? { ...styles.filterButton, ...styles.active }
-      : styles.filterButton;
-
-  const filterProgressButtonFinalClass =
-    activeFilter === TODOLIST_FILTERS.PROGRESS
-      ? { ...styles.filterButton, ...styles.active }
-      : styles.filterButton;
-
-  const filterDoneButtonFinalClass =
-    activeFilter === TODOLIST_FILTERS.DONE
-      ? { ...styles.filterButton, ...styles.active }
-      : styles.filterButton;
   return (
     <Stack direction={'row'} spacing={2} sx={styles.container}>
-      <Button
-        variant={'contained'}
-        sx={filterAllButtonFinalClass}
-        onClick={handleFilterAllClick}
-      >
-        {TODOLIST_FILTERS.ALL}
-      </Button>
-      <Button
-        variant={'contained'}
-        sx={filterTodoButtonFinalClass}
-        onClick={handleFilterTodoClick}
-      >
-        {TODOLIST_FILTERS.TODO}
-      </Button>
-      <Button
-        variant={'contained'}
-        sx={filterProgressButtonFinalClass}
-        onClick={handleFilterProgressClick}
-      >
-        {TODOLIST_FILTERS.PROGRESS}
-      </Button>
-      <Button
-        variant={'contained'}
-        sx={filterDoneButtonFinalClass}
-        onClick={handleFilterDoneClick}
-      >
-        {TODOLIST_FILTERS.DONE}
-      </Button>
+      {Object.entries(TODOLIST_FILTERS).map(([key, value]) => {
+        const handleFilterClick = () => dispatch(filterTasks(id, value));
+
+        const filterButtonFinalClass =
+          activeFilter === value
+            ? { ...styles.filterButton, ...styles.active }
+            : styles.filterButton;
+
+        return (
+          <Button
+            key={key}
+            variant={'contained'}
+            sx={filterButtonFinalClass}
+            onClick={handleFilterClick}
+          >
+            {value}
+          </Button>
+        );
+      })}
     </Stack>
   );
 };
