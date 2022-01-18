387f1e5847d2bd78740229a3772bb12766f215a4
Irina Martsulevich <malinovayairiska@gmail.com>
2022-01-18 16:10:23 +0300

    feat: add immer js, modify reducer's logic

--- src/store/reducers/tasksReducer.js
+++ src/store/reducers/tasksReducer.js
@@ -1,3 +1,4 @@
+import { produce } from 'immer';
 import { nanoid } from 'nanoid';
 
 import { ADD_TASK, DELETE_TASK } from '../actions/constants';
@@ -22,14 +23,20 @@ const initialState = [
   { id: nanoid(), title: 'TASK3', description: 'have to do' },
 ];
 
-export const tasksReducer = (state = initialState, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    case ADD_TASK:
-      return [...state, { ...payload, id: nanoid() }];
-    case DELETE_TASK:
-      return state.filter((task) => task.id !== payload.id);
-    default:
-      return state;
-  }
-};
+export const tasksReducer = (state = initialState, action) =>
+  produce(state, (draft) => {
+    const { type, payload } = action;
+    switch (type) {
+      case ADD_TASK: {
+        draft.push({ ...payload, id: nanoid() });
+        break;
+      }
+      case DELETE_TASK: {
+        const index = state.findIndex((task) => task.id === payload.id);
+        draft.splice(index, 1);
+        break;
+      }
+      default:
+        return state;
+    }
+  });
