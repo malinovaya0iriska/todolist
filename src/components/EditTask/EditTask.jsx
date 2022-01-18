cdd6ef5b01e0fed74a482779a651386de0be9170
Irina Martsulevich <malinovayairiska@gmail.com>
2022-01-18 16:53:36 +0300

    feat: create variables for routes,  start developing page for editing single Task

---
+++ src/components/EditTask/EditTask.jsx
@@ -0,0 +1,5 @@
+import { TaskSideBar } from '../TaskSideBar';
+
+export const EditTask = () => {
+  return <TaskSideBar />;
+};
---
+++ src/components/TaskSideBar/TaskSideBar.jsx
@@ -0,0 +1,8 @@
+import { Box } from '@mui/material';
+
+import { getStyles } from './getStyles';
+
+export const TaskSideBar = () => {
+  const styles = getStyles();
+  return <Box sx={styles.container}> </Box>;
+};
