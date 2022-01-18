bf1c2174b4c835c295b6a72b067d99a872877384
Irina Martsulevich <malinovayairiska@gmail.com>
2022-01-18 16:53:18 +0300

    feat: create variables for routes,  start developing page for editing single Task

---
+++ src/components/TaskSideBar/getStyles.js
@@ -0,0 +1,7 @@
+export const getStyles = () => ({
+  container: {
+    width: '300px',
+    height: '100vh',
+    background: '#ced4da',
+  },
+});
---
+++ src/components/TaskSideBar/index.js
@@ -0,0 +1,1 @@
+export { TaskSideBar } from './TaskSideBar';
